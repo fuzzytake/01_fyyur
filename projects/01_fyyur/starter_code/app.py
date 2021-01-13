dd5b28249d2ecc3e67c27b71b589a50edf501f78
fuzzytake <giuseppe_vullo@yahoo.it>
2021-01-13 17:51:20 +0100

    changed column name from genre to genres

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -41,7 +41,7 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    genre = db.Column(db.ARRAY(db.String()))
+    genres = db.Column(db.ARRAY(db.String()))
     address = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
---
+++ projects/01_fyyur/starter_code/migrations/versions/b349971cb5e3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b349971cb5e3
+Revises: 
+Create Date: 2020-12-27 19:04:27.530071
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b349971cb5e3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
